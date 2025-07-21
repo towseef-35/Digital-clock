0c698d117eef879e17ffccfd2839aa71fb380c4b
Towseef Ahmad Lone <152526807+towseef-35@users.noreply.github.com>
2025-07-21 22:27:58 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,13 @@
+function updateclock(){
+    const now=new Date();
+    let hours=now.getHours();
+    const meridian=hours>=12?"PM":"AM";
+    hours=hours%12 || 12;
+    hours=hours.toString().padStart(2,0);
+    const minutes=now.getMinutes().toString().padStart(2,0);
+    const seconds=now.getSeconds().toString().padStart(2,0);
+    const timestring=`${hours}:${minutes}:${seconds}:${meridian}`;
+    document.getElementById("clock").textContent=timestring
+}
+updateclock();
+setInterval(updateclock,1000);
